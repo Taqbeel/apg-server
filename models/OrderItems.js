4fbdb510ddc1c64eb2b699929244d264466d407b
Muhammad Taqbeel <53563490+Taqbeel@users.noreply.github.com>
2024-08-29 01:10:06 +0500

    orderitem model

--- models/OrderItems.js
+++ models/OrderItems.js
@@ -1,41 +1,46 @@
 module.exports = (sequelize, DataTypes) => {
   const OrderItems = sequelize.define("OrderItems", {
-    TaxCollection:{ type:DataTypes.JSON },
-    ProductInfo:{ type:DataTypes.JSON },
-    CurrencyCode:{ type:DataTypes.JSON },
-    TaxAmount:{ type:DataTypes.DOUBLE },
-    QuantityShipped:{ type:DataTypes.INTEGER },
-    BuyerRequestedCancel:{ type:DataTypes.JSON },
-    ItemPriceAmount:{ type:DataTypes.DOUBLE },
-    ASIN:{ type:DataTypes.STRING },
-    SellerSKU:{ type:DataTypes.STRING },
-    Title:{ type:DataTypes.STRING },
-    IsGift:{ type:DataTypes.BOOLEAN },
-    ConditionSubtypeId:{ type:DataTypes.STRING },
-    IsTransparency:{ type:DataTypes.BOOLEAN },
-    QuantityOrdered:{ type:DataTypes.INTEGER },
-    PromotionDiscountTax:{ type:DataTypes.DOUBLE },
-    ConditionId:{ type:DataTypes.STRING },
-    PromotionDiscountAmount:{ type:DataTypes.DOUBLE },
-    OrderItemId:{ type:DataTypes.STRING },
-    image:{
-      type:DataTypes.STRING,
-      defaultValue:'unfetched'
+    TaxCollection: { type: DataTypes.JSON },
+    ProductInfo: { type: DataTypes.JSON },
+    CurrencyCode: { type: DataTypes.JSON },
+    TaxAmount: { type: DataTypes.DOUBLE },
+    QuantityShipped: { type: DataTypes.INTEGER },
+    BuyerRequestedCancel: { type: DataTypes.JSON },
+    ItemPriceAmount: { type: DataTypes.DOUBLE },
+    ASIN: { type: DataTypes.STRING },
+    SellerSKU: { type: DataTypes.STRING },
+    Title: { type: DataTypes.STRING },
+    IsGift: { type: DataTypes.BOOLEAN },
+    ConditionSubtypeId: { type: DataTypes.STRING },
+    IsTransparency: { type: DataTypes.BOOLEAN },
+    QuantityOrdered: { type: DataTypes.INTEGER },
+    PromotionDiscountTax: { type: DataTypes.DOUBLE },
+    ConditionId: { type: DataTypes.STRING },
+    PromotionDiscountAmount: { type: DataTypes.DOUBLE },
+    OrderItemId: { type: DataTypes.STRING },
+    AmazonOrderId: { type: DataTypes.STRING },
+    itemFetched: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     },
-    weight:{ type:DataTypes.JSON },
-    dimensions:{ type:DataTypes.JSON },
-    isAlpha:{ 
-      type:DataTypes.BOOLEAN,
-      defaultValue:false
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: 'unfetched'
     },
-    alphaId:{ type:DataTypes.STRING },
-    priceFetched:{
-      type:DataTypes.BOOLEAN,
-      defaultValue:false
+    weight: { type: DataTypes.JSON },
+    dimensions: { type: DataTypes.JSON },
+    isAlpha: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     },
-    inventoryReceived:{
-      type:DataTypes.BOOLEAN,
-      defaultValue:false
+    alphaId: { type: DataTypes.STRING },
+    priceFetched: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    inventoryReceived: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     },
   })
   return OrderItems;
