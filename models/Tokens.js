fe5636867796f7d0e6e0bc53673b4805f19bb8ae
Muhammad Taqbeel <53563490+Taqbeel@users.noreply.github.com>
2024-08-29 01:09:44 +0500

    vendorName

--- models/Tokens.js
+++ models/Tokens.js
@@ -1,19 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
     const Tokens = sequelize.define("Tokens", {
-        access_token:{
-            type:DataTypes.TEXT,
+        access_token: {
+            type: DataTypes.TEXT,
             allowNull: false,
             validate: {
                 notEmpty: true
             }
         },
-        refresh_token:{
-            type:DataTypes.TEXT,
+        refresh_token: {
+            type: DataTypes.TEXT,
             allowNull: false,
             validate: {
                 notEmpty: true
             }
         },
+
+        vendorName: { type: DataTypes.STRING },
     })
     return Tokens;
 }
