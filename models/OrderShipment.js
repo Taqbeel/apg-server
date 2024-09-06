4d564758ac3409eea98ebe5ef8f73a395ba235be
Muhammad Taqbeel <53563490+Taqbeel@users.noreply.github.com>
2024-09-07 01:22:25 +0500

    ordershipment

--- models/OrderShipment.js
+++ models/OrderShipment.js
@@ -1,16 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
   const OrderShipment = sequelize.define("OrderShipment", {
-    shipmentId:{
-      type:DataTypes.STRING,
+    shipmentId: {
+      type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
-    trackingId:{ type:DataTypes.STRING },
-    packageClientReferenceId:{ type:DataTypes.STRING },
-    document:{ type:DataTypes.TEXT },
-    promise:{ type:DataTypes.JSON },
-    totalCharge:{ type:DataTypes.STRING },
-    chargeUnit:{ type:DataTypes.STRING },
+    trackingId: { type: DataTypes.STRING },
+    packageClientReferenceId: { type: DataTypes.STRING },
+    document: { type: DataTypes.TEXT },
+    promise: { type: DataTypes.JSON },
+    totalCharge: { type: DataTypes.STRING },
+    chargeUnit: { type: DataTypes.STRING },
+    format: { type: DataTypes.STRING },
   })
   return OrderShipment;
 }
